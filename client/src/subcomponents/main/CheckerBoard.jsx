a55d988faa7fa29dd22cd19f851fce8a43001dc4
Davyd Zakorchennyi <Diza41a@mail.com>
2022-06-21 23:26:19 -0400

    Implemented MVP checker game logic

---
+++ client/src/subcomponents/main/CheckerBoard.jsx
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from 'react';
+
+// import CheckerBoard from './CheckerBoard.js';
+const { checkerMethods } = require('./checkerMethods.js');
+
+const [emptyField, whitePiece, whiteQueen, blackPiece, blackQueen, markedField] = [0, 1, 2, 3, 4, 5];
+const pieces = [null, 'white-piece', 'white-queen', 'black-piece', 'black-queen'];
+
+export default function CheckerBoard() {
+  const [isLoading, toggleLoading] = useState(true);
+  const [board, setBoard] = useState();
+  const [turn, toggleTurn] = useState('white');
+  const [piecesToEat, setPiecesToEat] = useState(null); // { piece: { row, col }, eadables: [{row, col}, ...] }
+  let currentPiece = null;
+
+  // Sound effects
+  const moveSound = new Audio('../assets/move.mp3');
+  const newQueenSound = new Audio('../assets/queened.wav');
+  const destroyPieceSound = new Audio('../assets/eaten.wav');
+
+  // componentDidMount
+  // Give state a board matrix from the server
+  useEffect(() => {
+    toggleLoading(false);
+    setBoard(checkerMethods.generateBoard(8));
+  }, []);
+
+  // componentDidUpdate()
+  // Clear potential moves
+  useEffect(() => {
+    document.querySelectorAll('.potential-move').forEach((el) => {
+      el.classList.remove('potential-move');
+    });
+  })
+
+  // Visual presentation of a checker piece
+  const generatePiece = (value, rowI, colI) => {
+    // If value is not a checker piece, don't generate anything
+    if (value <= 0 || value >= 5) {
+      return null;
+    } else {
+      return <div className={pieces[value]} data-row={rowI} data-col={colI}></div>;
+    }
+  }
+
+  // Get all of the potential moves for selected piece
+  const getMoves = (e) => {
+    // Clear all higlighted fields
+    document.querySelectorAll('.potential-move').forEach((field) => {
+      field.classList.remove('potential-move');
+    });
+
+    // Collect piece information
+    const el = e.target;
+    let rowI = parseInt(el.getAttribute('data-row'), 10);
+    let colI = parseInt(el.getAttribute('data-col'), 10);
+    const piece = board[rowI][colI];
+
+    // If there's a current obligation to eat a piece,
+    // and the wrong piece is selected, do not proceed
+    if (piecesToEat !== null
+      && (piecesToEat.piece.row !== rowI || piecesToEat.piece.col !== colI)) {
+        return;
+    }
+
+    const moves = [];
+    // Do not proceed when it's opponent's move
+    if (turn === 'white' && (piece === blackPiece || piece === blackQueen)
+    || turn === 'black' && (piece === whitePiece || piece === whiteQueen)) {
+      return;
+    }
+
+    // Get upward moveset for white pieces, or black queens
+    if (piece === whitePiece || piece === whiteQueen || piece === blackQueen) {
+      if (piecesToEat === null) {
+        moves.push(checkerMethods.getUpMoves(board, rowI, colI));
+      }
+      moves.push(checkerMethods.getUpHitMoves(board, rowI, colI));
+    }
+    // Get downward moveset for black pieces, or white queens
+    if (piece === blackPiece || piece === blackQueen || piece === whiteQueen) {
+      if (piecesToEat === null) {
+        moves.push(checkerMethods.getDownMoves(board, rowI, colI));
+      }
+      moves.push(checkerMethods.getDownHitMoves(board, rowI, colI));
+    }
+    // Highlight possible moves
+    moves.flat().forEach((move) => {
+      document.querySelector(`td[data-row="${move.row}"][data-col="${move.col}"]`).classList.add('potential-move');
+    });
+    // Highlight current piece
+    if (board[rowI][colI] !== emptyField) {
+      document.querySelector(`td[data-row="${rowI}"][data-col="${colI}"]`).classList.add('potential-move');
+      currentPiece = { row: rowI, col: colI };
+    } else {
+      currentPiece = null;
+    }
+  }
+
+  // Move a piece / Destroy an enemy
+  const move = (e) => {
+    // Only allow movement on the marked fields
+    if (!e.target.classList.contains('potential-move')) {
+      return;
+    }
+
+    // Collect piece information
+    const el = e.target;
+    let rowI = parseInt(el.getAttribute('data-row'), 10);
+    let colI = parseInt(el.getAttribute('data-col'), 10);
+    const moveMeta = (checkerMethods.movePiece(board, currentPiece, { row: rowI, col: colI }));
+    // If move occurred, update the board
+    // Perform all necessary checks and effects
+    if (moveMeta.moved) {
+      setBoard(moveMeta.board);
+      // Play corresponding sound effect
+      if (moveMeta.queened) {
+        newQueenSound.play();
+      } else if (moveMeta.justAte) {
+        destroyPieceSound.play();
+      } else {
+        moveSound.play();
+      }
+
+      // If just ate/destroyed a piece, check for more 'food'
+      if (moveMeta.justAte && !moveMeta.queened) {
+        // Collect potential hit moves in all direction of the piece
+        const hitMovesUp = checkerMethods.getUpHitMoves(moveMeta.board, rowI, colI);
+        const hitMovesDown = checkerMethods.getDownHitMoves(moveMeta.board, rowI, colI);
+        let hitMoves = [];
+        if (hitMovesUp !== null) {
+          hitMoves.push(hitMovesUp);
+        }
+        if (hitMovesDown !== null) {
+          hitMoves.push(hitMovesDown);
+        }
+        hitMoves = hitMoves.flat();
+        // If obligation for a hit move exists,
+        // don't toggle turn, or allow any other moves...
+        if (hitMoves.length > 0) {
+          setPiecesToEat({ piece: { row: rowI, col: colI }, eadables: hitMoves });
+          return;
+        } else {
+          // Otherwise, reset the obligations
+          setPiecesToEat(null);
+        }
+      }
+      turn === 'white' ? toggleTurn('black') : toggleTurn('white');
+    }
+  }
+
+  // Move handling logics unified under a single manager
+  const moveManager = (e) => {
+    if (currentPiece !== null) {
+      move(e);
+      currentPiece = null;
+    }
+    if (currentPiece === null) {
+      getMoves(e);
+    }
+  };
+
+  // Generate visual representation of the board
+  if (isLoading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+  const visualBoard = (
+    <table className="checker-board">
+      <tbody>
+        {board.map((row, rowI) => {
+          return <tr key={rowI}>{board[rowI].map((col, colI) => {
+            let classes = rowI % 2 === 0 ? 'checker-field even' : 'checker-field odd';
+            return <td className={classes} key={colI}  data-row={rowI} data-col={colI} onClick={moveManager} >
+              {generatePiece(col, rowI, colI)}
+            </td>;
+          })}</tr>
+        })}
+      </tbody>
+    </table>
+  );
+
+  return (
+    <div>{visualBoard}</div>
+  );
+};
---
+++ client/src/subcomponents/main/checkerMethods.js
@@ -0,0 +1,239 @@
+const [emptyField, whitePiece, whiteQueen, blackPiece, blackQueen, markedField] = [0, 1, 2, 3, 4, 5];
+
+// Generate conditional board (two-dimensional array)
+const generateBoard = (size) => {
+  if (size !== 10 && size !== 12) {
+    size = 8;
+  }
+  let board = new Array(size);
+    for (let rowI = 0; rowI < size; rowI += 1) {
+      board[rowI] = new Array(size);
+      for (let colI = 0; colI < size; colI += 1) {
+        if (rowI === size / 2 || rowI === size / 2 - 1 ||
+        (rowI % 2 === 0 && colI % 2 === 0) || (rowI % 2 !== 0 && colI % 2 !== 0)) {
+          board[rowI][colI] = 0;
+        } else if (rowI < size / 2) {
+          board[rowI][colI] = 3;
+        } else {
+          board[rowI][colI] = 1;
+        }
+      }
+    }
+  return board;
+};
+
+const clearMarkedCells = (board) => {
+  // Clear up any marked cells (unoptimized)
+  for (const row of board) {
+    for (const col of row) {
+      if (col === markedField) {
+        col = emptyField;
+      }
+    }
+  }
+};
+
+const getUpMoves = (board, rowI, colI) => {
+  if (board[rowI][colI] !== 0 && board[rowI][colI] !== 3) {
+    // Clear up any marked cells (unoptimized)
+    for (const row of board) {
+      for (const col of row) {
+        if (col=== markedField) {
+          col = emptyField;
+        }
+      }
+    }
+    const availableCells = [];
+
+    if (rowI > 0) {
+      // up left
+      if (colI > 0 && board[rowI - 1][colI - 1] === 0) {
+        availableCells.push({ row: rowI - 1, col: colI - 1 });
+      }
+      // up right
+      if (colI < board.length - 1 && board[rowI - 1][colI + 1] === 0) {
+        availableCells.push({ row: rowI - 1, col: colI + 1 });
+      }
+    }
+
+    return availableCells;
+  }
+  return null;
+};
+
+const getUpHitMoves = (board, rowI, colI) => {
+  const currentPiece = board[rowI][colI];
+  if (currentPiece !== emptyField && currentPiece !== blackPiece) {
+    // Clear up any marked cells (unoptimized)
+    for (const row of board) {
+      for (const col of row) {
+        if (col === markedField) {
+          col = emptyField;
+        }
+      }
+    }
+    const availableCells = [];
+
+    if (rowI > 1) {
+      // Up left
+      if (colI > 1) {
+        if (board[rowI - 2][colI - 2] === emptyField) {
+          const leftPiece = board[rowI - 1][colI - 1];
+
+          const canEatBlack = (currentPiece === whitePiece || currentPiece === whiteQueen)
+            && (leftPiece === blackPiece || leftPiece === blackQueen);
+          const canEatWhite = (currentPiece === blackPiece || currentPiece === blackQueen)
+            && (leftPiece === whitePiece || leftPiece === whiteQueen);
+          if (canEatBlack || canEatWhite) {
+            availableCells.push({ row: rowI - 2, col: colI - 2});
+          }
+        }
+      }
+      // Up right
+      if (colI < board.length - 2) {
+        if (board[rowI - 2][colI + 2] === emptyField) {
+          const rightPiece = board[rowI - 1][colI + 1];
+
+          const canEatBlack = (currentPiece === whitePiece || currentPiece === whiteQueen)
+            && (rightPiece === blackPiece || rightPiece === blackQueen);
+          const canEatWhite = (currentPiece === blackPiece || currentPiece === blackQueen)
+            && (rightPiece === whitePiece || rightPiece === whiteQueen);
+          if (canEatBlack || canEatWhite) {
+            availableCells.push({ row: rowI - 2, col: colI + 2});
+          }
+        }
+      }
+    }
+
+    return availableCells;
+  }
+  return null;
+}
+
+const getDownMoves = (board, rowI, colI) => {
+  if (board[rowI][colI] !== 0 && board[rowI][colI] !== 1) {
+    // Clear up any marked cells (unoptimized)
+    for (const row of board) {
+      for (const col of row) {
+        if (col=== markedField) {
+          col = emptyField;
+        }
+      }
+    }
+    const availableCells = [];
+    if (rowI < board.length - 1) {
+      // down left
+      if (colI > 0 && board[rowI + 1][colI - 1] === 0) {
+        availableCells.push({ row: rowI + 1, col: colI - 1 });
+      }
+      // down right
+      if (colI < board.length - 1 && board[rowI + 1][colI + 1] === 0) {
+        availableCells.push({ row: rowI + 1, col: colI + 1 });
+      }
+    }
+
+    return availableCells;
+  }
+  return null;
+};
+
+const getDownHitMoves = (board, rowI, colI) => {
+  const currentPiece = board[rowI][colI];
+  if (currentPiece !== emptyField && currentPiece !== whitePiece) {
+    // Clear up any marked cells (unoptimized)
+    for (const row of board) {
+      for (const col of row) {
+        if (col === markedField) {
+          col = emptyField;
+        }
+      }
+    }
+    const availableCells = [];
+
+    if (rowI < board.length - 2) {
+      // Down left
+      if (colI > 1) {
+        if (board[rowI + 2][colI - 2] === emptyField) {
+          const leftPiece = board[rowI + 1][colI - 1];
+
+          const canEatBlack = (currentPiece === whitePiece || currentPiece === whiteQueen)
+            && (leftPiece === blackPiece || leftPiece === blackQueen);
+          const canEatWhite = (currentPiece === blackPiece || currentPiece === blackQueen)
+            && (leftPiece === whitePiece || leftPiece === whiteQueen);
+          if (canEatBlack || canEatWhite) {
+            availableCells.push({ row: rowI + 2, col: colI - 2});
+          }
+        }
+      }
+      // Down right
+      if (colI < board.length - 2) {
+        if (board[rowI + 2][colI + 2] === emptyField) {
+          const rightPiece = board[rowI + 1][colI + 1];
+
+          const canEatBlack = (currentPiece === whitePiece || currentPiece === whiteQueen)
+            && (rightPiece === blackPiece || rightPiece === blackQueen);
+          const canEatWhite = (currentPiece === blackPiece || currentPiece === blackQueen)
+            && (rightPiece === whitePiece || rightPiece === whiteQueen);
+          if (canEatBlack || canEatWhite) {
+            availableCells.push({ row: rowI + 2, col: colI + 2});
+          }
+        }
+      }
+    }
+
+
+    return availableCells;
+  }
+  return null;
+};
+
+const destroyField = (board, rowI, colI) => {
+  const updatedBoard = board.slice();
+  updatedBoard[rowI][colI] = emptyField;
+  return updatedBoard;
+};
+
+const movePiece = (board, currentCell, moveCell) => {
+  // Do nothing if the same field was selected
+  if (currentCell.row === moveCell.row && currentCell.col === moveCell.col) {
+    return { moved: false, justAte: false, board: board, queened: false };
+  }
+
+  let updatedBoard = board.slice();
+  const moveMeta = { moved: true, justAte: false, queened: false };
+  const piece = updatedBoard[currentCell.row][currentCell.col];
+  updatedBoard[currentCell.row][currentCell.col] = emptyField;
+  if (piece === whitePiece && moveCell.row === 0) {
+    updatedBoard[moveCell.row][moveCell.col] = whiteQueen;
+    moveMeta.queened = true;
+  } else if (piece === blackPiece && moveCell.row === board.length - 1) {
+    updatedBoard[moveCell.row][moveCell.col] = blackQueen;
+    moveMeta.queened = true;
+  } else {
+    updatedBoard[moveCell.row][moveCell.col] = piece;
+  }
+  moveMeta.board = updatedBoard;
+
+  // If a piece should be eaten
+  if (Math.abs(currentCell.row - moveCell.row) === 2
+    && Math.abs(currentCell.col - moveCell.col) === 2) {
+      const destroyRow = (currentCell.row + moveCell.row) / 2;
+      const destroyCol = (currentCell.col + moveCell.col) / 2;
+      moveMeta['board'] = destroyField(updatedBoard, destroyRow, destroyCol);
+      moveMeta['justAte'] = true;
+  }
+
+  return moveMeta;
+};
+
+module.exports = {
+  checkerMethods: {
+    generateBoard,
+    getUpMoves,
+    getUpHitMoves,
+    getDownMoves,
+    getDownHitMoves,
+    movePiece,
+    destroyField,
+  }
+};
